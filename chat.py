cab9ad6b47b1b3994357866ac71b4bb04e3e0d25
vasanth <vasanthv@gmail.com>
2023-09-25 22:11:16 +0200

    version 1 - 5 personality questions answered

--- chat.py
+++ chat.py
@@ -1,62 +1,80 @@
-# Credit : https://ipip.ori.org/New_IPIP-50-item-scale.htm
+# Credit : https://ipip.ori.org/New_IPIP-50-objQnA-scale.htm
 import openai
 import pandas as pd
-openai.api_key = "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"
-#openai.api_key = "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"
-model_engine = "text-davinci-003"  
+#use constants to store the API key and engine name
+openai.api_key = os.getenv("OPENAI_API_KEY")
+engine = "gpt-4"
 
 
-
-# read excel in to a dataframe
+import os
 import pandas as pd
 
-df = pd.read_excel(r'C:\Users\vasanthv\Downloads\TedoneItemAssignmentTable30APR21.xlsx', sheet_name='data_nodupes')
-prompt_list = []
+file_path = r"C:\Users\vasanthv\Downloads\TedoneItemAssignmentTable30APR21.xlsx"
 
-def retrieve_row_data(df, index):
-    qNo = df.loc[index, 'Number'] # get the title from the data frame
-    instrument = df.loc[index, 'instrument'] # get the year from the data frame
-    alpha = df.loc[index, 'alpha'] # get the author from the data frame
-    key = df.loc[index, 'key'] # get the summary from the data frame
-    question = df.loc[index, 'text'] # get the summary from the data frame
-    label = df.loc[index, 'label'] # get the summary from the data frame
-    list = [qNo, instrument, alpha, key, question, label]
-    return list
+if os.path.exists(file_path):
+    df = pd.read_excel(file_path, sheet_name='data_nodupes')
+else:
+    print("File not found at the specified path.")
 
-#Store column names in a list
-column_names = df.columns.tolist()
-#row_count = len(df.index)
-row_count = 5
-# iterate through the rows of the dataframe and print values in column question
-for i in range(row_count):
-    row_data = retrieve_row_data(df, i)
-    prompt_list = prompt_list + [row_data[4]]
-#print(prompt_list)
+column_list=[]
+row_count = len(df.index)
+column_list = df.columns.tolist()
 
-def constructPrompt (df) : 
-    prompt = f"I will say {row_count} things about you and you must sys_ans_text agree or disagree for all of them Sort your sys_ans_text in the same order as the question Here are the questions I am a person who\n"
-    for i in range(row_count):
-        row_data = retrieve_row_data(df, i)
-        prompt = prompt + str(row_data[4]) + "\n"
-    return prompt
-prompt = constructPrompt(df)
+#define variable for each of the column
+column_list[0] = 'number'
+column_list[1] = 'objQnA'
+column_list[2] = 'alpha'
+column_list[3] = 'key'
+column_list[4] = 'question'
+column_list[5] = 'label'
+
+#create a class object to store values in each of the column across total row count
+class ObjQnA:
+    def __init__(self, number, objQnA, alpha, key, question, label):
+        self.number = number
+        self.objQnA = objQnA
+        self.alpha = alpha
+        self.key = key
+        self.question = question
+        self.label = label
+
+#Iterate through each row and store the values in the objQnA object
+def load_objQnA(df, row_count):
+    objQnA_list = []
+    for i in range(10):
+        objQnA = ObjQnA(df.iloc[i,0],df.iloc[i,1],df.iloc[i,2],df.iloc[i,3],df.iloc[i,4],df.iloc[i,5])
+        objQnA_list.append(objQnA)
+    return objQnA_list
+
+objQnA_list = load_objQnA(df, row_count)
+#print(objQnA_list)
+
+#function to access ObjQnA object and create a list of questions, adding all the questions to the list
+def get_questions(objQnA_list):
+    question_list = []
+    for objQnA in objQnA_list:
+        question_list.append(objQnA.question)
+    return question_list
+
+question_list = get_questions(objQnA_list)
+prompt1 = f"I will ask you,questions.Answer with either agree or disagree for each of them."
+prompt1 = str(prompt1)
+#Function to create a prompt string concatenating a string and all values in the question_list
+def create_prompt(question_list):
+    prompt = "This is a list of questions.Answer them in the same order as they appear."
+    for question in question_list:
+        prompt += question
+    return str(prompt)
+
+prompt2 = create_prompt(question_list)
+
+#create a prompt string concatenating prompt1 and prompt2, avoid Type Error
+prompt = prompt1 + prompt2
 print(prompt)
-#completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024,n=1,stop=None, listerature=0.5)
-#response = completion.choices[0].text
-#response_list = response.split("\n")
-# find the number of sentences in a text
-list_ans = []
-sys_ans_text = "Disagree, Agree, Disagree, Disagree, Disagree"
-def count_sentences(text):
-    count = 0
-    list_ans = []
-    for i in range(len(text)):
-        if text[i] == ',':
-            count += 1
-            list_ans = list_ans + [text[i]]
-    return count+1, list_ans
 
-sys_ans_texts_count, list_ans = count_sentences(sys_ans_text)
-
-for i in range(sys_ans_texts_count):
-    print(f"Question {prompt_list[i]} sys_ans_text : {list_ans[i]}")
+response = openai.Completion.create(
+  model="gpt-3.5-turbo-instruct",
+  prompt=prompt
+)
+response_message = response['choices'][0]['text']
+print(response_message)
