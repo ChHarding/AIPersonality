559e5454a388347a746a106a5bb3a25024790321
vasanth <vasanthv@gmail.com>
2023-09-25 18:05:08 +0200

    IPIP resource for questions

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+# Credit : https://ipip.ori.org/New_IPIP-50-item-scale.htm
 import openai
 openai.api_key = "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"
 model_engine = "text-davinci-003"  
