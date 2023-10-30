b91a27f0f57250801cf34224e460912e88c4e628
vasanth <vasanthv@gmail.com>
2023-10-30 16:02:57 +0100

    Code to extract test dataset

---
+++ Clean_Explore_Master.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import shelve as sh
+import tqdm
+
+file_path = r'data\IPIP-FFM-data-8Nov2018\data-final.csv'
+shelve_folder = r'Shelve'
+pickle_folder = r'Pickle'
+requriedColumns = ['EXT1', 'EXT2', 'EXT3', 'EXT4', 'EXT5', 'EXT6', 'EXT7', 'EXT8', 'EXT9', 'EXT10', 'EST1', 'EST2', 'EST3', 'EST4', 'EST5', 'EST6', 'EST7', 'EST8', 'EST9', 'EST10', 'AGR1', 'AGR2', 'AGR3', 'AGR4', 'AGR5', 'AGR6', 'AGR7', 'AGR8', 'AGR9', 'AGR10', 'CSN1', 'CSN2', 'CSN3', 'CSN4', 'CSN5', 'CSN6', 'CSN7', 'CSN8', 'CSN9', 'CSN10', 'OPN1', 'OPN2', 'OPN3', 'OPN4', 'OPN5', 'OPN6', 'OPN7', 'OPN8', 'OPN9', 'OPN10']
+positiveKeys = ['AGR2','AGR4','AGR6','AGR8','AGR9','AGR10','CSN1','CSN3','CSN5','CSN7','CSN9','CSN10','EST2','EST4','EXT1','EXT3','EXT5','EXT7','EXT9','OPN1','OPN3','OPN5','OPN7','OPN8','OPN9','OPN10']
+negativeKeys = ['AGR1','AGR3','AGR5','AGR7','CSN2','CSN4','CSN6','CSN8','EST1','EST3','EST5','EST6','EST7','EST8','EST9','EST10','EXT2','EXT4','EXT6','EXT8','EXT10','OPN2','OPN4','OPN6']
+
+print('Loading Dataset to Dataframe...')
+df = pd.read_csv(file_path, sep='\t', usecols=requriedColumns, engine='c')
+
+# change the type of header to string
+df.columns = df.columns.astype(str)
+actualRows = len(df.index)
+print('--',actualRows, 'records in the dataset before cleaning. Total Records:', len(df.index))
+
+# remove records that has 0 in any of the answers
+df = df.dropna()
+print('--',actualRows - len(df.index), 'records removed from the dataset due to missing values. Total Records:', len(df.index))
+
+# Define the threshold for frequency (e.g., 70%)
+threshold = 0.50
+tqdm.tqdm.pandas(desc="Calculating Rowfrequency to remove records with overused values.")
+# Calculate the frequency of each value in each row without progress bar
+row_frequencies = df.progress_apply(lambda x: x.value_counts() / len(x), axis=1)
+
+# Check if any value occurs more than the threshold in each row
+rows_to_remove = row_frequencies.max(axis=1) > threshold
+
+# Remove the rows where any value occurs more than the threshold
+IPIPDatasetWithScores = df[~rows_to_remove]
+
+# Print the length of DataFrame with rows removed
+records_removed = len(df.index) - len(IPIPDatasetWithScores.index)
+print('--',records_removed, 'records removed from the dataset.Applying a threshold of', threshold, 'for frequency. Total Records:', len(IPIPDatasetWithScores.index))
+
+#vectorized function on every row of the df_cleaned dataframe, pass dataframe to the function
+# get the values for the positive and negative keys
+positiveValues = IPIPDatasetWithScores[positiveKeys].values
+negativeValues = IPIPDatasetWithScores[negativeKeys].values
+
+# calculate scores for each personality trait
+totalagrreableValues = np.sum(positiveValues[:, :6], axis=1) + np.sum(6 - negativeValues[:, :4], axis=1)
+totconscientiousValues = np.sum(positiveValues[:, 6:12], axis=1) + np.sum(6 - negativeValues[:, 5:8], axis=1)
+totemotionalStabilityValues = np.sum(positiveValues[:, 12:14], axis=1) + np.sum(6 - negativeValues[:, 9:16], axis=1)
+totextraversionValues = np.sum(positiveValues[:, 14:19], axis=1) + np.sum(6 - negativeValues[:, 17:21], axis=1)
+totintellectValues = np.sum(positiveValues[:, 19:26], axis=1) + np.sum(6 - negativeValues[:, 22:24], axis=1)
+
+# insert new columns for scores
+IPIPDatasetWithScores['agreeablenessScore'] = totalagrreableValues
+IPIPDatasetWithScores['conscientiousnessScore'] = totconscientiousValues
+IPIPDatasetWithScores['emotionalStabilityScore'] = totemotionalStabilityValues
+IPIPDatasetWithScores['extraversionScore'] = totextraversionValues
+IPIPDatasetWithScores['intellectScore'] = totintellectValues
+
+# find outliers of personality scores using z-score
+# define the z-score threshold
+z_score_threshold = 2
+
+# calculate z-scores of the personality scores
+z_scores = (IPIPDatasetWithScores[['agreeablenessScore', 'conscientiousnessScore', 'emotionalStabilityScore', 'extraversionScore', 'intellectScore']] - IPIPDatasetWithScores[['agreeablenessScore', 'conscientiousnessScore', 'emotionalStabilityScore', 'extraversionScore', 'intellectScore']].mean()) / IPIPDatasetWithScores[['agreeablenessScore', 'conscientiousnessScore', 'emotionalStabilityScore', 'extraversionScore', 'intellectScore']].std()
+
+# calculate the absolute z-scores
+abs_z_scores = abs(z_scores)
+
+# find the rows with outliers
+outlier_rows = (abs_z_scores > z_score_threshold).any(axis=1)
+
+# print the outliers and the number of outliers
+outliers = IPIPDatasetWithScores[outlier_rows]
+print("Number of outliers:", len(outliers))
+print("Outliers:")
+print(outliers)
+
+# remove the outliers from the original dataframe
+IPIPDatasetWithScores = IPIPDatasetWithScores[~outlier_rows]
+print('Row count after removing outliers:', len(IPIPDatasetWithScores))
+
+# store the dataset with scores in a Pickl file under the relevant folder pickle_folder
+IPIPDatasetWithScores.to_pickle(pickle_folder + r'\IPIPDatasetWithScores.pkl')
+print("IPIP Dataset with scores stored in pickle file")
+
+# store the dataset with scores in a shelved file under the relevant folder shelve_folder
+shelve_file = sh.open(shelve_folder + r'\IPIPDatasetWithScores.shelve')
+shelve_file['IPIPDatasetWithScores'] = IPIPDatasetWithScores
+shelve_file.close()
+print("IPIP Dataset with scores stored in shelved file")
---
+++ Extract_datasets.py
@@ -0,0 +1,59 @@
+import pickle as pkl
+import pandas as pd
+
+# load the dataset with scores from the Pickle file
+pickle_folder = 'Pickle'
+with open(f'{pickle_folder}/IPIPDatasetWithScores.pkl', 'rb') as f:
+    IPIPDatasetWithScores = pkl.load(f)
+
+# define a function to change all answers to text values
+def change_answers_to_text(df):
+    return df.replace({1: 'Disagree Strongly', 2: 'Disagree Moderately', 3: 'Neither Agree nor Disagree', 4: 'Agree Moderately', 5: 'Agree Strongly'})
+
+# replace all question codes in the data frames with Question(column D) in in IPIPQuestions dataframe by matching the question code with Code(column B) 
+IPIPQuestions = pd.read_excel('data/IPIP-FFM-data-8Nov2018/IPIPQuestionaire.xlsx', usecols=['Code', 'Question'], index_col='Code').squeeze().to_dict()
+
+# define a function to replace question codes with question text in a dataframe
+def replace_question_codes(df):
+    return df.rename(columns=IPIPQuestions)
+
+def createTrainingDataset(df,trait,type):
+    row_count = len(df.index)
+    column_count = 50
+    role_system_content = 'xx'
+    if type == 1:
+        recordType='high'
+    else:
+        recordType='low'
+    # create a training example for each row in the dataframe. Each row is a training example. Each training example has 3 messages - system, user and assistant. system message is role_system_content, user message is the column header and assistant message is the value in the cell
+    for row in range(row_count):
+        for column in range(column_count):
+            if df.iloc[row, column] != role_system_content:
+                trainingRecord = '{"messages": [{"role": "system", "content":'+ role_system_content +'}, {"role": "user", "content": "' + df.columns[column] + '"}, {"role": "assistant", "content": "' + df.iloc[row, column] + '"}]}'
+                # write the training record to a text file under a folder 'Training_Records. First time create the folder and file, after that append to the file
+                with open(f'Training_Records/{recordType}_{trait}_scores.txt', 'a') as f:
+                    f.write(trainingRecord + '\n')
+    print(f'Training records created for {trait} trait with {recordType} scores')
+
+        
+# create dataframes dynamically using a loop and store in lists
+traits = ['agreeableness', 'conscientiousness', 'emotionalStability', 'extraversion', 'intellect']
+# remove records that has 0 in any of the answers
+print('Row count before removing records with 0 in any of the answers:', len(IPIPDatasetWithScores))
+
+IPIPDatasetWithScores = IPIPDatasetWithScores[~(IPIPDatasetWithScores == 0).any(axis=1)]
+print ('Row count after removing records with 0 in any of the answers:', len(IPIPDatasetWithScores))
+
+
+for trait in traits:
+    high_scores = change_answers_to_text(IPIPDatasetWithScores.nlargest(100, f'{trait}Score'))
+    low_scores = change_answers_to_text(IPIPDatasetWithScores.nsmallest(100, f'{trait}Score'))
+
+    globals()[f'high_{trait}_scores'] = replace_question_codes(high_scores)
+    print('Dataframe for high scores:', globals()[f'high_{trait}_scores'].describe())
+
+    globals()[f'low_{trait}_scores'] = replace_question_codes(low_scores)
+    print('Dataframe for low scores:', globals()[f'low_{trait}_scores'].describe())
+    
+    createTrainingDataset(globals()[f'high_{trait}_scores'],trait,1)
+    createTrainingDataset(globals()[f'low_{trait}_scores'],trait,0)
